297a051e5d4afdb114cf3e3bc551422d8979b5b0
Benjamin Vera <benjamin.vera.toledo@gmail.com>
2025-10-26 17:05:34 -0300

    Refactor WebDriver setup in test_main.py
    Updated Chrome WebDriver initialization to use Service from webdriver_manager.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_open_python_org():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--disable-dev-tools")
-    options.add_argument("--disable-infobars")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get("https://www.python.org")
     assert "Python" in driver.title
     driver.quit()
