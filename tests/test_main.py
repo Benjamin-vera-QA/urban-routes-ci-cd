9865118a52f29c1aba7f6dd923434f3ef9eb2646
Benjamin Vera <benjamin.vera.toledo@gmail.com>
2025-10-26 06:40:49 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller
 
 def test_open_python_org():
-    driver = webdriver.Chrome()
+
+    chromedriver_autoinstaller.install()
+
+    options = Options()
+    options.add_argument("--headless")  # Ejecuta sin ventana
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.python.org")
     assert "Python" in driver.title
     driver.quit()
