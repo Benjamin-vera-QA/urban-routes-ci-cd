7fbb20dcd5f0d7f5dad5cceb82fcb57f2ae93c4b
Benjamin Vera <benjamin.vera.toledo@gmail.com>
2025-10-26 06:19:50 -0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+def test_open_python_org():
+    driver = webdriver.Chrome()
+    driver.get("https://www.python.org")
+    assert "Python" in driver.title
+    driver.quit()
