986d653fca1302ee5f29f8d295169fd38828d30f
Benjamin Vera <benjamin.vera.toledo@gmail.com>
2025-10-26 17:01:16 -0300

    Refactor Selenium test by removing chromedriver installer
    Removed chromedriver_autoinstaller installation and added additional Chrome options for improved headless operation.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
 
 def test_open_python_org():
-
-    chromedriver_autoinstaller.install()
-
     options = Options()
-    options.add_argument("--headless")  # Ejecuta sin ventana
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--disable-infobars")
 
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.python.org")
